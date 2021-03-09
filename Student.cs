efafa748998a3306676049dc04950a6535ad18c2
ttytty1 <43213398+ttytty1@users.noreply.github.com>
2021-03-09 10:31:06 +0800

    added a file

---
+++ Student.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace 教学事务管理系统
+{
+    public class Student
+    {
+        public string Xh
+        { get; set; }
+        public string Xm
+        { get; set; }
+        public string Xb
+        { get; set; }
+        public string Csrq
+        { get; set; }
+        public string Jg
+        { get; set; }
+        public string Sjhm
+        { get; set; }
+        public string Yxh
+        { get; set; }
+        public string Mn
+        { get; set; }
+
+    }
+}
